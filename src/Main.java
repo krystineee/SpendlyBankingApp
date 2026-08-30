bbe2dfcb487ee06673aaf4cca75b480a0f0b2e7a
shibaa <92931538+krystineee@users.noreply.github.com>
2026-08-31 03:44:00 +0800

    Remove debug print statement from Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import userInterface.UserLogin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("DEBUG - password env var: " + System.getenv("SPENDLY_DB_PASSWORD"));
         new UserLogin();
     }
 }
